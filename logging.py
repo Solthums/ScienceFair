fd19058853261f66cbf4fc0f9e0072c4bed1ec8d
Solthums <solthums@gmail.com>
2013-12-26 21:09:41 -0800

    Update logging.py

--- logging.py
+++ logging.py
@@ -1,32 +1,31 @@
-# import necessary libraries
-import serial
-import time
-
-# Create a serial connection
-connection = serial.Serial('/dev/tty.usbmodem1411', 115200)
+connection = serial.Serial('/dev/tty.usbmodem1421', 115200) 
 # Wait for the serial connection to resolve
 
 force = 0
 pulse = 0
 
-threshold = 640
+threshold = 620
 isBreathing = False
 
 while True:
-	line = connection.readline()
-	parts = line.split(":")
-	if len(parts) == 2:
-		if parts[0] == "0":
-			force = int(parts[1])
-		if parts[0] == "1":
-			pulse = int(parts[1])
-		# here is where your code goes
-		if force < threshold:
-			if isBreathing:
-				# continue
-			else:
-				isBreathing = True
-		else:
-			# do something else
-		print "Force is " + str(force) + " and pulse is " + str(pulse)
+    line = connection.readline()
+    parts = line.split(":")
+    if len(parts) == 2
+        if parts[0] == "0":
+            if parts[1] != '0':
+                force = int(parts[1])
+                print force
+        if parts[0] == "1":
+            if parts[1] != '0':
+                pulse = int(parts[1])
+                print force
+        if force <= threshold:
+            isBreathing = True
+            print "You took a breath at Force of: " + str(force)
+        else:
+	        isBreathing = False
+	        print "no breath at Force of: " + str(force)
+    
+	print "Force is " + str(force) + " and pulse is " + str(pulse)
+
 
